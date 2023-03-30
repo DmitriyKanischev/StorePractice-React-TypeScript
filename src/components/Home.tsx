daf714dad9c95901ea30c6df0ff9ec75d76819eb
DmitriyKanischev <116942865+DmitriyKanischev@users.noreply.github.com>
2023-03-30 21:07:13 +0400

    home - flexbox added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,16 +4,16 @@ import React from "react";
 export function Home() {
     return (
         <div 
-            className="absolute left-0 right-0 bottom-0 top-0 bg-slate-300 ">
+            className="flex w-full min-h-screen bg-slate-300 ">
                 <div 
-                    className="absolute left-0 right-0 bottom-0 top-0 bg-slate-100 w-[50%] flex justify-center items-center">
+                    className="bg-slate-100 w-[50%] flex justify-center items-center">
                         <img 
                             className="rounded-2xl w-3/4 border-solid border-2 border-slate-500 shadow-lg shadow-slate-500/50" 
                             src="https://optomby.com/wp-content/uploads/2019/11/Internet-magazin-odezhdy-1.jpg" 
                             alt="shop welcome" />
                 </div>
                 <div 
-                    className="absolute left-[50%] right-0 bottom-0 top-0 bg-slate-100 w-[50%] shadow-lg shadow-slate-500/50">
+                    className="bg-slate-100 w-[50%] shadow-lg shadow-slate-500/50">
                         <h1 
                             className="text-blue-400 font-bold text-7xl text-left mx-8 my-10 drop-shadow-lg"
                             >Buy more - 
